b7c4d3f771f521751b8a8216efa3fbe124f5fb8b
PedroAlbert147 <152289606+PedroAlbert147@users.noreply.github.com>
2026-05-03 15:38:55 -0300

    Add files via upload

--- Atividade 5/main.c
+++ Atividade 5/main.c
@@ -6,11 +6,12 @@
 
 #define BTN 13
 #define LED 12
-#define DEBOUNCE 200000
-#define TIME_ON 10000000 //10s
+#define DEBOUNCE 200000       //200ms
+#define TIME_ON 10000000      //10s
 
-int64_t last_change_time = 0;
-int last_btn_val = 1;
+int64_t last_signal_change = 0;  
+int current_stable_state = 1;    
+int last_stable_state = 1;       // ultimo debounce state
 int led_state = 0;
 int64_t led_on_time = 0;
 
@@ -22,23 +23,37 @@ void app_main() {
 
   while (1) {
 
-    int btn_val = gpio_get_level(BTN);
+    int raw_btn = gpio_get_level(BTN);
     int64_t time = esp_timer_get_time();
 
-    if (btn_val == 0 && last_btn_val == 1 && (time - last_change_time > DEBOUNCE)) {
+    
+    static int last_raw_btn = 1;
+    if (raw_btn != last_raw_btn) {
+      last_signal_change = time;
+      last_raw_btn = raw_btn;
+    }
 
-      led_state = !led_state; //tem q ser toggle
+    
+    if ((time - last_signal_change) >= DEBOUNCE) {
+      current_stable_state = raw_btn;
+    }
+
+    
+    if (current_stable_state == 0 && last_stable_state == 1) {
+      
+      led_state = !led_state; //eh toggle
       gpio_set_level(LED, led_state);
-      last_change_time = time;
       if (led_state == 1) {
         led_on_time = time;
       }
     }
+
+    last_stable_state = current_stable_state;
+
+    //contagem regressiva
     if (led_state == 1 && (time - led_on_time > TIME_ON)) {
       led_state = 0;
       gpio_set_level(LED, 0);
     }
-
-    last_btn_val = btn_val;
   }
 }
